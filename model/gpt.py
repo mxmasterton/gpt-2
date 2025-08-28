fb1b66d3b43ef59d23ef70510bbe0011bcb7c0cb
Max Masterton <maxmasterton.work@gmail.com>
2025-08-28 17:16:11 +0100

    Added loading of weights from a huggingface model

--- model/gpt.py
+++ model/gpt.py
@@ -44,3 +44,53 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd)
         ))
         self.ln_head = nn.Linear(config.n_head, config.vocab_size, bias=False)
+        
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """ loads pre-trained GPT-2 model weights from huggingface """
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: {}".format(model_type))
+        
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2":         dict(n_layer=12, n_head=12, n_embd=768),    # 124M parameters
+            "gpt2-medium":  dict(n_layer=24, n_head=16, n_embd=1024),   # 350M parameters
+            "gpt2-large":   dict(n_layer=36, n_head=20, n_embd=1280),   # 774M parameters
+            "gpt2-xl":      dict(n_layer=48, n_head=25, n_embd=1600)    # 1558M parameters
+        }[model_type]
+        config_args["vocab_size"] = 50257 # always 50257 for GPT model checkpoints
+        config_args["block_size"] = 1024 # always 1024 for GPT model checkpoints
+        
+        # create a from-scratch initalised model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")] # discard this mask, not a parameter
+        
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        
+        sd_hf = model_hf.state_dict()
+        sd_keys_hf = model_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        # we have to transpose certain weights when we import them
+        transposed = ["attn.c_attn.weight", "attn.c_proj.weight", "mlp.c_fc.weight", "mlp.c_proj.weight"]
+        assert len(sd_keys_hf) == len(sd_keys), "mismatched keys: {} != {}".format(len(sd_keys_hf), len(sd_keys))
+        for k in sd_keys_hf:
+            if any(k.endwith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        
+        return model
